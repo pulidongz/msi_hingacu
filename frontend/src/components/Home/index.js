fe52351521d2442a5ed3075d0229643b5dd2ea67
pulidongz <paogravoso@gmail.com>
2021-07-24 12:20:36 +0800

    Added more basemaps and overlays

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -20,31 +20,109 @@ import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
 
 import { useHistory } from 'react-router-dom';
 
-import { MapContainer, TileLayer, LayersControl } from 'react-leaflet'
+import { MapContainer, TileLayer, WMSTileLayer, LayersControl, FeatureGroup, Popup, Circle, Reactangle } from 'react-leaflet'
+import { LayerGroup } from 'leaflet';
 
 const theme = createMuiTheme();
 
 export default function Home () {
 	let history = useHistory();
+	const karagatanURL = 'http://167.86.124.21:8080/geoserver/karagatan/wms';
 
 	return(
 		<Grid container direction="row"
 		justify="center"
 		alignItems="center">
-			<MapContainer className="leaflet_home_map" center={[12.599512, 121.984222]} zoom={6} scrollWheelZoom={false} style={{ height: "100vh" }}>
+			<MapContainer className="leaflet_home_map" center={[12.599512, 121.984222]} zoom={6} scrollWheelZoom={true} style={{ height: "100vh", width: "100vw"}}>
 				<LayersControl position="topright" collapsed={false}>
-						<LayersControl.BaseLayer checked name="ESRI World Imagery">
-							<TileLayer
-								attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-								url="http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
-							/>
-						</LayersControl.BaseLayer>
-						<LayersControl.BaseLayer name="ESRI World Street Map">
-							<TileLayer
-								attribution='Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012'
-								url="http://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}"
-							/>
-						</LayersControl.BaseLayer>
+					<LayersControl.BaseLayer checked name="NAMRIA Basemap">
+						<TileLayer
+							attribution='© <a href="https:/www.geoportal.gov.ph">Geoportal Philippines</a>'
+							url='http://basemapserver.geoportal.gov.ph/tiles/v2/PGP/{z}/{x}/{y}.png'
+						/>
+					</LayersControl.BaseLayer>
+					<LayersControl.BaseLayer name="Google Map">
+						<TileLayer
+							attribution='© Google <a href="https://developers.google.com/maps/terms">Terms of Use</a>'
+							url='http://mt0.google.com/vt/lyrs=y&hl=en&x={x}&y={y}&z={z}&s=Ga'
+						/>
+					</LayersControl.BaseLayer>
+					<LayersControl.BaseLayer name="ESRI World Imagery">
+						<TileLayer
+							attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+							url='http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
+						/>
+					</LayersControl.BaseLayer>
+					<LayersControl.BaseLayer name="ESRI World Street Map">
+						<TileLayer
+							attribution='Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012'
+							url='http://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}'
+						/>
+					</LayersControl.BaseLayer>
+					
+					<LayersControl.Overlay checked name="National Integrated Protected Areas System">
+						<WMSTileLayer
+							url='http://167.86.124.21:8080/geoserver/karagatan/wms'
+							layers= 'karagatan:Protected Areas'
+							version= '2.15.1'
+							transparent= {true}
+							format= 'image/png'
+						/>
+					</LayersControl.Overlay>
+					<LayersControl.Overlay checked name="Kalayaan Island Group">
+						<WMSTileLayer
+							url='http://167.86.124.21:8080/geoserver/karagatan/wms'
+							layers= 'karagatan:kalayaanisgroup'
+							version= '2.15.1'
+							transparent= {true}
+							format= 'image/png'
+						/>
+					</LayersControl.Overlay>
+					<LayersControl.Overlay checked name="Philippine Extended Continental Shelf">
+						<WMSTileLayer
+							url='http://167.86.124.21:8080/geoserver/karagatan/wms'
+							layers= 'PhilECS'
+							version= '2.15.1'
+							transparent= {true}
+							format= 'image/png'
+						/>
+					</LayersControl.Overlay>
+					<LayersControl.Overlay name="Fisheries Management Areas">
+						<WMSTileLayer
+							url='http://167.86.124.21:8080/geoserver/karagatan/wms'
+							layers= 'karagatan:fma'
+							version= '2.15.1'
+							transparent= {true}
+							format= 'image/png'
+						/>
+					</LayersControl.Overlay>
+					<LayersControl.Overlay name="Philippine Territorial/Internal Waters">
+						<WMSTileLayer
+							url='http://167.86.124.21:8080/geoserver/karagatan/wms'
+							layers= 'karagatan:territorialwaters'
+							version= '2.15.1'
+							transparent= {true}
+							format= 'image/png'
+						/>
+					</LayersControl.Overlay>
+					<LayersControl.Overlay name="Municipal waters-archipelagic principle">
+						<WMSTileLayer
+							url='http://167.86.124.21:8080/geoserver/karagatan/wms'
+							layers= 'karagatan:municipal_waters'
+							version= '2.15.1'
+							transparent= {true}
+							format= 'image/png'
+						/>
+					</LayersControl.Overlay>
+					<LayersControl.Overlay name="Municipal waters-mainland principle(hypothetical)">
+						<WMSTileLayer
+							url='http://167.86.124.21:8080/geoserver/karagatan/wms'
+							layers= 'karagatan:munwaters_mainland'
+							version= '2.15.1'
+							transparent= {true}
+							format= 'image/png'
+						/>
+					</LayersControl.Overlay>
 			</LayersControl>
 		</MapContainer>
 		</Grid>
