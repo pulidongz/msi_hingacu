7944675c041ed6bd3f2f1498450382c23ef8584a
pulidongz <paogravoso@gmail.com>
2023-01-08 17:03:38 +0800

    Added theme for MUI

--- frontend-vite/src/main.tsx
+++ frontend-vite/src/main.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ThemeProvider, createTheme } from '@mui/material'
+
 import App from './App'
 import './index.css'
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#319795'
+    }
+  }
+})
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 )
